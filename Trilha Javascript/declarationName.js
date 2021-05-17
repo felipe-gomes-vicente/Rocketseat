6bd93759f30480f58597313620e542ee3f0c9d1e
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-05-17 17:05:52 -0300

    declarationName

---
+++ Trilha Javascript/declarationName.js
@@ -0,0 +1,27 @@
+/**
+ * Variáveis e tipos de dados
+ * declaração or declaration
+ */
+var name 
+
+// assignment or atribuição de valores
+ name = 'Felipe'
+
+// que tipo de dado foi colocado na variável
+console.log(name)
+console.log(typeof name)
+
+// agrupamento de declarações
+let age, isHuman
+
+age = 20
+isHuman = true
+
+// multiplos argumentos na função
+console.log(name, age, isHuman)
+
+// escrita de texto + variáveis  concatenando valores
+console.log('o ' + name + ' tem ' + age + ' anos')
+
+// interpolando valores com template literals or template strings
+console.log(`o ${name} tem ${age} anos.`)
