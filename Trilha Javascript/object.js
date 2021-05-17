74c40e33bd2062a442ff9fee2b0435b521a017b5
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-05-17 17:14:45 -0300

    object.js

--- Trilha Javascript/object.js
+++ Trilha Javascript/object.js
@@ -16,3 +16,15 @@ console.log({
     console.log('andar')
   }
 }) 
+
+const person = {
+  name: 'Felipe', // propriedade 
+  age: 27,
+  weight: 88.6,
+  isAdmin: true
+}
+
+console.log(person)
+console.log(person.name) // aqui foi chamado apenas o valor da propriedade name
+console.log(person.age) // aqui foi chamado apenas o valor da propriedade age
+console.log(`${person.name} tem ${person.age} anos.`)
