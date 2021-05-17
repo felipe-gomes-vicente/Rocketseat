bb88d714e9f0bce9850b7969019a39d87c506e92
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-05-17 14:03:26 -0300

    object.js

---
+++ Trilha Javascript/object.js
@@ -0,0 +1,18 @@
+/*
+Object
+
+*Propriedade / atributos
+* Funcionalidades / Métodos
+
+{ propriedade: "valor" }
+*/
+
+console.log({}) // objecto vazio
+
+console.log({
+  name: "Felipe", // propriedade 
+  idade: 26,
+  andar: function(){
+    console.log('andar')
+  }
+}) 
