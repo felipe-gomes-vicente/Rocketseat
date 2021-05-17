cdd9ec24dfb4fd27b9873b27ff187e4d938b1254
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-05-17 14:29:21 -0300

    variaveis.js

---
+++ Trilha Javascript/variaveis.js
@@ -0,0 +1,30 @@
+/**
+ * #Variáveis
+ * 
+ * Nomes simbólicos para receber algum valor 
+ * Atalhos de código
+ * Identificadores
+ * 3 palavras reservadas para criar uma variavel 
+ 
+    * var
+    * let
+    * const
+ */
+
+//var
+var clima = "quente"
+clima = "frio"
+
+console.log(clima)
+
+// let (forma moderna de declarar uma variavel sempre preferivel utiliza-la)
+let oClima = "quente"
+oClima = "frio"
+
+console.log(oClima)
+
+//const 
+const temperatura = "quente"
+temperatura = "frio"
+
+console.log(temperatura) // constante não é possivel alterar valor
