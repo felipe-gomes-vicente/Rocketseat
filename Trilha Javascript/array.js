ba3f21f66f6704070091f8b7691ef8cafbe2c781
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-05-17 17:30:04 -0300

    array.js

--- Trilha Javascript/array.js
+++ Trilha Javascript/array.js
@@ -13,3 +13,19 @@ console.log([
   2, 
   3
 ])
+
+const animals = [
+  'Lion',
+  'Monkey',
+  {
+    name: 'Cat',
+    age: 3
+  }
+]
+
+// acessar valores dentro do Array 
+console.log(animals)
+console.log(animals[0]) // lista começa a contar do zero 
+console.log(animals.length) // qual seria o valor
+console.log(animals[2]) // ao chamar o animal retornar um objeto
+console.log(animals[2].name) // retorna a propriedade do objeto
