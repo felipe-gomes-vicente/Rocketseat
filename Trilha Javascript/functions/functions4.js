5497fc6a5d1cc842a4f637125f9f763b8050e81c
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-05-18 12:41:48 -0300

    function.js

--- Trilha Javascript/functions/functions4.js
+++ Trilha Javascript/functions/functions4.js
@@ -31,3 +31,24 @@ function sayMyName(name) {
 
 sayMyName()
 */
+
+/*
+  Function() Constructor
+
+  * expressão new
+  * criar um novo objeto
+  * this keyword
+*/
+
+function Person(name) {
+  this.name = name
+  this.walk = function() {
+    return this.name + " está andando"
+  }
+}
+const felipe = new Person("Felipe")
+const yume = new Person("Yume")
+console.log(felipe)
+console.log(yume)
+console.log(felipe.walk())
+console.log(yume.walk())
