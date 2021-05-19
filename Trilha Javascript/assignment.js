4c5f1d5205d10a2087975401efcaf73acf8cdedd
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-05-19 12:16:22 -0300

    assignment

---
+++ Trilha Javascript/assignment.js
@@ -0,0 +1,25 @@
+// Operadores de Atribuição (Assignment)
+let x
+
+// assignment
+x = 1
+
+// addition assignment
+// x = x + 2
+ x += 2 
+
+//subtraction assignment
+// x = x - 1
+ x -= 1
+
+// multiplication assignment
+// x = x * 2
+  x *= 2
+
+// division assignment
+// x = x / 2
+ x /= 2
+
+// remainder assignment
+// x %= 2
+  x **= 2
--- Trilha Javascript/operadoresDeComparacao.js
+++ Trilha Javascript/operadoresDeComparacao.js
@@ -12,3 +12,11 @@ let two = 2
  console.log(one != two)
  console.log(one != 1)
  console.log(one != "1")
+
+// === estritamente igual a
+console.log( one === "1")
+console.log( one === 1)
+
+// !== estritamente diferente de 
+console.log( two !== "2")
+console.log( two !== 2)
